60643c9e508e90fad9bd62beb123d2f65f245f6c
Shunya Kitada <syun.kitada@gmail.com>
2015-11-01 19:27:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Fablib gluster
+
+This is fablib for gluster.
+
+
+## Example vars
+``` bash
+gluster:
+  common:
+    volume: common
+    brick: /export/common
+    hosts: ${#node_map.gluster.hosts}
+    host: ${#node_map.gluster.hosts.0}
+```
+
+## Example fabscript
+``` python
+# coding: utf-8
+
+from fabkit import task, api
+from fablib.gluster import Gluster
+
+gluster = Gluster()
+
+
+@task
+def setup0():
+    gluster.setup()
+
+
+@task
+@api.serial
+def setup1_peer():
+    gluster.setup_peer()
+
+
+@task
+def setup2_volume():
+    gluster.setup_volume()
+```
---
+++ gluster.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+from fabkit import filer, sudo, env, Service
+from fablib.base import SimpleBase
+
+
+class Gluster(SimpleBase):
+    def __init__(self):
+        self.data_key = 'gluster'
+        self.data = {
+        }
+
+        self.services = [
+            'glusterd',
+        ]
+
+        self.packages = [
+            'glusterfs-server',
+        ]
+
+    def init_after(self):
+        for cluster in self.data.values():
+            if env.host in cluster['hosts']:
+                self.data['gluster_cluster'] = cluster
+                break
+
+    def setup(self):
+        data = self.init()
+        cluster = data['gluster_cluster']
+
+        filer.template('/etc/yum.repos.d/glusterfs-epel.repo')
+
+        self.install_packages()
+        self.start_services().enable_services()
+
+        filer.mkdir(cluster['brick'])
+
+        Service('firewalld').stop().disable()
+
+    def setup_peer(self):
+        """
+        This method should be called in sirial task
+        """
+        data = self.init()
+        cluster = data['gluster_cluster']
+
+        for host in cluster['hosts']:
+            if host != env.host:
+                sudo('gluster peer probe {0}'.format(host))
+
+    def setup_volume(self):
+        data = self.init()
+        cluster = data['gluster_cluster']
+        if cluster['hosts'][0] != env.host:
+            return
+
+        sudo('gluster volume info {0} || gluster volume create '
+             '{0} replica 2 {1}:{2} {3}:{2} force'.format(
+                 cluster['volume'], cluster['hosts'][0], cluster['brick'], cluster['hosts'][1]))
+        sudo('gluster volume info {0} | grep Started || gluster volume start {0}'.format(
+            cluster['volume']))
