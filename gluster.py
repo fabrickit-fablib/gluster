36a1ffbff5db2f203ffa22b9401fe1024cbafa73
Shunya Kitada <syun.kitada@gmail.com>
2016-02-11 23:20:15 +0900

    update

--- gluster.py
+++ gluster.py
@@ -10,52 +10,61 @@ class Gluster(SimpleBase):
         self.data = {
         }
 
-        self.services = [
-            'glusterd',
-        ]
+        self.services = {
+            'CentOS Linux 7.*': [
+                'glusterd',
+            ]
+        }
 
-        self.packages = [
-            'glusterfs-server',
-        ]
+        self.packages = {
+            'CentOS Linux 7.*': [
+                'centos-release-gluster36',
+                'glusterfs-server',
+            ]
+        }
 
     def init_after(self):
-        for cluster in self.data.values():
+        for cluster in self.data.get('clusters', {}).values():
             if env.host in cluster['hosts']:
-                self.data['gluster_cluster'] = cluster
-                break
+                self.data.update(cluster)
 
     def setup(self):
         data = self.init()
-        cluster = data['gluster_cluster']
-
-        filer.template('/etc/yum.repos.d/glusterfs-epel.repo')
-
-        self.install_packages('--enablerepo glusterfs-epel')
-        self.start_services().enable_services()
-
-        filer.mkdir(cluster['brick'])
 
         Service('firewalld').stop().disable()
 
+        self.install_packages()
+        self.start_services().enable_services()
+
+        for volume in data['volume_map'].values():
+            filer.mkdir(volume['brick'])
+
     def setup_peer(self):
         """
-        This method should be called in sirial task
+        require serial task.
         """
-        data = self.init()
-        cluster = data['gluster_cluster']
 
-        for host in cluster['hosts']:
+        data = self.init()
+        for host in data['hosts']:
             if host != env.host:
                 sudo('gluster peer probe {0}'.format(host))
 
     def setup_volume(self):
+        """
+        require serial task.
+        """
+
         data = self.init()
-        cluster = data['gluster_cluster']
-        if cluster['hosts'][0] != env.host:
+        if data['hosts'][0] != env.host:
             return
 
-        sudo('gluster volume info {0} || gluster volume create '
-             '{0} replica 2 {1}:{2} {3}:{2} force'.format(
-                 cluster['volume'], cluster['hosts'][0], cluster['brick'], cluster['hosts'][1]))
-        sudo('gluster volume info {0} | grep Started || gluster volume start {0}'.format(
-            cluster['volume']))
+        for volume in data['volume_map'].values():
+            bricks = ''
+            for host in data['hosts']:
+                bricks += '{0}:{1} '.format(host, volume['brick'])
+            sudo('gluster volume info {0[name]} || gluster volume create '
+                 '{0[name]} replica {0[replica]} {1} force'.format(
+                     volume, bricks))
+            sudo('gluster volume info {0[name]} | grep Started'
+                 ' || gluster volume start {0[name]}'.format(
+                     volume))
--- test-repo/fabscript/test/base.py
+++ test-repo/fabscript/test/base.py
@@ -1,11 +1,23 @@
 # coding: utf-8
 
-from fabkit import task
-from fablib.sample import Sample
-
-sample = Sample()
+from fabkit import task, parallel
+from fablib.gluster import Gluster
 
 
 @task
+@parallel
 def setup():
-    sample.setup()
+    gluster = Gluster()
+    gluster.setup()
+
+
+@task
+def setup1_peer():
+    gluster = Gluster()
+    gluster.setup_peer()
+
+
+@task
+def setup2_volume():
+    gluster = Gluster()
+    gluster.setup_volume()
